6fd067cd6db76761d9ded3b185102c73d487d914
ray <ohmyray@163.com>
2021-07-01 19:56:55 +0800

    测试路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,39 @@
-import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes: RouteConfig[] = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
   },
   {
-    path: '/login',
-    name: 'Login',
+    path: "/login",
+    name: "Login",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue'),
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
   },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   // base: import.meta.env.BASE_URL,
   routes,
+});
+
+// 注册路由
+router.beforeEach((to, from, next) => {
+  let token = localStorage.getItem("r-token");
+  // if (!token && from.path !== "/login") next({ path: "/login" })
+
+  console.log("(to, from, next)", token, to.path, from.path, next);
+  // next(); // 放行
 });
 
 export default router;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,86 @@
 <template>
-  <div class="login">
-    <h1>This is an about page</h1>
-    <el-button>ceui</el-button>
+  <div class="r-login-container">
+    <el-form
+      :model="userForm"
+      :rules="rules2"
+      status-icon
+      ref="ruleForm2"
+      label-position="left"
+      label-width="0px"
+      class="r-login-form"
+    >
+      <h3 class="title">系统登录</h3>
+      <el-form-item prop="username">
+        <el-input
+          type="text"
+          v-model="userForm.username"
+          auto-complete="off"
+          placeholder="用户名"
+        ></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input
+          type="password"
+          v-model="userForm.password"
+          auto-complete="off"
+          placeholder="密码"
+        ></el-input>
+      </el-form-item>
+      <el-checkbox v-model="checked" class="rememberme">记住密码</el-checkbox>
+      <el-form-item style="width: 100%">
+        <el-button
+          type="primary"
+          style="width: 100%"
+          @click="handleSubmit"
+          :loading="logining"
+          >登录</el-button
+        >
+      </el-form-item>
+    </el-form>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+interface IUser {
+  username: string;
+  password: string;
+}
+
+@Component
+export default class Login extends Vue {
+  private userForm: IUser = {
+    username: "",
+    password: "",
+  };
+  private checked: boolean = false;
+  private logining: boolean = false;
+  private rules2: any = {
+    username: [
+      { required: true, message: "please enter your account", trigger: "blur" },
+    ],
+    password: [
+      { required: true, message: "enter your password", trigger: "blur" },
+    ],
+  };
+  handleSubmit() {
+    console.log("this.$refs.ruleForm2", this.$refs.ruleForm2);
+  }
+}
+</script>
+<style lang="scss" scoped>
+.r-login-container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+
+}
+.r-login-form {
+  width: 20vw;
+  box-sizing: border-box;
+  padding-top: 15%;
+}
+</style>
